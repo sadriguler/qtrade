faab47cfc50ce65dbb6895e3783863204c2a0274
Sadri Guler <sadriguler@protonmail.com>
2025-07-27 19:22:56 +0200

    repo file organization, portfolio manager, config for data path

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from os import getcwd
+from os.path import abspath
+from os.path import dirname
+
+script_path = abspath(__file__)
+dir_path = dirname(script_path)
+PROJECT_PATH = f'{dir_path}/..'
+
+DATA_PATH = f'{PROJECT_PATH}/data/' 
+DATA_PATH_USER = f'{DATA_PATH}/user_data/'
