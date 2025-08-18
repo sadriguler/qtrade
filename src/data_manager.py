14bd008af7e8752bb96b8b76415959bf36346e62
Sadri Guler <sadriguler@protonmail.com>
2025-08-18 23:56:45 +0200

    data manager updated for better data download

--- src/data_manager.py
+++ src/data_manager.py
@@ -51,6 +51,51 @@ def find_date_after_days(start_date_str, num_days):
 def get_today():
     return strftime('%Y-%m-%d',gmtime())
 
+def download_tefas_data(start: str, end_date: str, asset: str):
+    data_list = []
+    tefas_data = Crawler()
+    while days_between(start, end_date) > 90:
+        temporary_end_date = find_date_after_days(start, 90)
+        data = tefas_data.fetch(start=start, end=temporary_end_date, name=asset, columns=["code", "date", "price", "stock", "number_of_investors"])
+        data_list.append(data)
+        start = find_date_after_days(temporary_end_date, 1)
+        print(f"Downloading data from start: {start}, to temporary: {temporary_end_date}")
+    else: 
+        if days_between(start, end_date) > 10:
+            data = tefas_data.fetch(start=start, end=end_date, name=asset, columns=["code", "date", "price", "stock", "number_of_investors"])
+            data_list.append(data)
+        else:
+            start = find_date_after_days(end_date, -10)
+            print(f"Downloading data from start: {start}, to end: {end_date}")
+            data = tefas_data.fetch(start=start, end=end_date, name=asset, columns=["code", "date", "price", "stock", "number_of_investors"])
+            data_list.append(data)
+        
+    print('Data was fetched from the API! Converting to the desired format...')
+    
+    if len(data_list) > 1:
+        for i, data in enumerate(data_list): 
+            data_list[i] = data.sort_values('date').reset_index(drop = True)
+        data = pd.concat(data_list).reset_index(drop = True)
+        data['Date'] = pd.to_datetime(data['date'])
+        data.drop(columns='date', inplace=True)
+        data.set_index('Date', inplace=True)
+        return data
+    elif len(data_list) == 1:
+        if len(data_list[0]) == 0:
+            print('ERROR: The requested data was not downloaded. There is a problem!')
+            print('Although the data_list has a length of 1, its first element is empty.')
+            print(f'The downloaded data is {data_list[0]=}')
+            return 0
+        else: 
+            data = data_list[0].sort_values('date').reset_index(drop = True)
+            data['Date'] = pd.to_datetime(data['date'])
+            data.drop(columns='date', inplace=True)
+            data.set_index('Date', inplace=True)
+            return data
+    else:   
+        print('ERROR: The requested data was not downloaded. There is a problem!')
+        return 0
+
 def download_data(asset: str = 'MAC', market: str = 'TEFAS', start: str = '2025-01-01'):
     """
     Downloads the data by using the related API for the interested asset.
@@ -65,50 +110,20 @@ def download_data(asset: str = 'MAC', market: str = 'TEFAS', start: str = '2025-
         
     """
     start = str(start)
-    tefas_data = Crawler()
     end_date  = strftime('%Y-%m-%d',gmtime())
-    data_list = []
     if market == "TEFAS":    
-        while days_between(start, end_date) > 90:
-            temporary_end_date = find_date_after_days(start, 90)
-            data = tefas_data.fetch(start=start, end=temporary_end_date, name=asset, columns=["code", "date", "price", "stock", "number_of_investors"])
-            data_list.append(data)
-            start = find_date_after_days(temporary_end_date, 1)
-            print(f"Downloading data from start: {start}, to temporary: {temporary_end_date}")
-        else: 
-            if days_between(start, end_date) > 1:
-                data = tefas_data.fetch(start=start, end=end_date, name=asset, columns=["code", "date", "price", "stock", "number_of_investors"])
-                data_list.append(data)
-        print('Data was fetched from the API!')
-        if len(data_list) > 1:
-            for i, data in enumerate(data_list): 
-                data_list[i] = data.sort_values('date').reset_index(drop = True)
-            data = pd.concat(data_list).reset_index(drop = True)
-            data['Date'] = pd.to_datetime(data['date'])
-            data.drop(columns='date', inplace=True)
-            data.set_index('Date', inplace=True)
-            return data
-        elif len(data_list) == 1:
-            if len(data_list[0]) == 0:
-                print('ERROR: The requested data was not downloaded. There is a problem!')
-                return 0
-            else: 
-                return data_list[0]
-        else:   
-            print('ERROR: The requested data was not downloaded. There is a problem!')
-            return 0
+        data = download_tefas_data(start = start, end_date = end_date, asset = asset)
     elif market == 'NYSE' or market == 'NASDAQ':
         data = yf.download(asset, period = 'max', auto_adjust=False, start=start)
-        return data
     elif market == 'FOREX':
         if asset in YF_ASSET_NAMES:
             data = yf.download(YF_ASSET_NAMES[asset], period = 'max', auto_adjust=False, start=start)
         else:
             data = yf.download(YF_ASSET_NAMES[asset], period = 'max', auto_adjust=False, start=start)
-        return data
     else: 
-        print("I'm able to download data only from TEFAS!")
-        
+        print("Market name should be FOREX, NYSE, or TEFAS!")
+        return 0
+    return data
         
 
 def get_data(fund: str = 'MAC', market: str = 'TEFAS'):
